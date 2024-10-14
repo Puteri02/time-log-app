a6668f96e061f68acb4805c7daa8f49eb7e334d7
pu3 <puterifiqrina02@gmail.com>
2024-10-14 10:33:05 +0800

    Update : Added data table and CSS

--- src/Components/TimeLogList.js
+++ src/Components/TimeLogList.js
@@ -1,28 +1,31 @@
 import React from "react";
+import TimeLogItem from "./TimeLogItem";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import TimeLogItem from "./TimeLogItem";
+import './TimeLogList.css'
 
 const TimeLogList = ({ timeLogs, deleteTimeLog }) => {
   return (
-    <div className="card" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+    <div className="card" style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%'}}>
       <DataTable
         value={timeLogs}
         paginator rows={5} rowsPerPageOptions={[5, 10, 15]} // Pagination
-        tableStyle={{ maxWidth: '80rem'}}
+        tableStyle={{width: '60rem'}}
+        removableSort
+        //responsiveLayout="stack" // responsiveness
       >
         {/*Display log table with sort*/}
-        <Column field="task" header="Task" sortable style={{ width: '20%', textAlign: 'center', justifyContent: 'center'}} />
-        <Column field="startTime" header="Start Time" sortable style={{ width: '20%', textAlign: 'center'}} />
-        <Column field="endTime" header="End Time" sortable style={{ width: '20%', textAlign: 'center'}} />
-        <Column field="date" header="Date" sortable style={{ width: '20%', textAlign: 'center'}} />
-        <Column field="duration" header="Duration" sortable style={{ width: '20%', textAlign: 'center' }} />
+        <Column field="task" header="Task" sortable style={{ width: '20%'}} />
+        <Column field="startTime" header="Start Time" sortable style={{ width: '25%'}} />
+        <Column field="endTime" header="End Time" sortable style={{ width: '25%'}} />
+        <Column field="date" header="Date" sortable style={{ width: '25%'}} />
+        <Column field="duration" header="Duration" sortable style={{ width: '30%'}} />
         <Column body={(rowData) => (
             // Delete button
             <TimeLogItem log={rowData} deleteTimeLog={deleteTimeLog} />
           )}
           header="Action"
-          style={{ width: '50%' }}
+          style={{ width: '50%', justifyContent: 'center'}}
         />
       </DataTable>
     </div>
